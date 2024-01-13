9b06e215f01bad664f502d606f36c49cf6dc8de8
Erick Lopez <93694994+ericklopezdev@users.noreply.github.com>
2024-01-12 22:44:23 -0600

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,33 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      textColor: {
+        primary: "var(--gray-12)",
+        secondary: "var(--gray-11)",
+        tertiary: "var(--gray-9)",
+        brand: "var(--brand)",
+        link: "var(--blue-10)",
+      },
+      backgroundColor: {
+        primary: "var(--gray-1)",
+        secondary: "var(--gray-4)",
+        secondaryA: "var(--gray-a4)",
+        tertiary: "var(--gray-3)",
+      },
+      keyframes: {
+        in: {
+          "0%": { transform: "translateY(18px)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+        "in-reverse": {
+          "0%": { transform: "translateY(-18px)", opacity: "0" },
+          "100%": { transform: "translateY(0px)", opacity: "1" },
+        },
+      },
+      animation: {
+        in: "in .6s both",
+        "in-reverse": "in-reverse .6s both",
+      }
     },
   },
   plugins: [],
